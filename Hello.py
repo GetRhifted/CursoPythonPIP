d319b0a0c5a8f7023bb764a9e541573be6e8be01
Michael <rhift.michael@gmail.com>
2023-04-08 18:25:03 -0500

    Ultimas funciones de practica definidas

--- Hello.py
+++ Hello.py
@@ -17,3 +17,24 @@ def reversed_word(text):
     return texto_inverso
 
 print(reversed_word('Estoy Aprendiendo a Programar'))
+
+def mayor_num(n1,n2):
+    if n1 > n2:
+        return n1
+    else:
+        return n2
+
+print(mayor_num(45,56))
+
+
+def mayot_num_3(n1,n2,n3):
+    if n1 > n2 and n1 > n3:
+        return n1
+    elif n2 > n1 and n2 > n3:
+        return n2
+    else:
+        return n3
+    
+print(mayot_num_3(67,97,46))
+
+
