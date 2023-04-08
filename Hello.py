1881ae2f974276f543d3517d9046cc7680d3f015
Michael <rhift.michael@gmail.com>
2023-04-08 18:17:37 -0500

    Añadí funciones al archivo Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,19 @@
 print('Hola desde mi maquina')
+
+def counter_word(text):
+    word = 'a'
+    counter = 0
+    for i in text:
+        if i.lower() == word:
+            counter += 1
+    return counter
+
+print(counter_word('Azulaaaaaaa'))
+
+def reversed_word(text):
+    texto_inverso = ''
+    for l in text:
+        texto_inverso = l + texto_inverso
+    return texto_inverso
+
+print(reversed_word('Estoy Aprendiendo a Programar'))
