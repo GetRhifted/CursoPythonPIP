8dab0f38b11f1c07c01b8590bd9fe35835d54710
Michael <rhift.michael@gmail.com>
2023-04-10 12:48:07 -0500

    Inluí por primera vez la libreria de Pandas para comparar su desempeño con mi codigo inicial

--- app/main.py
+++ app/main.py
@@ -1,19 +1,29 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  '''
+  
   data = list(filter(lambda x : x['Continent'] == 'South America', data))
   countries = list(map(lambda x : x['Country/Territory'], data))
   percentage = list(map(lambda x : x['World Population Percentage'], data))
+ 
+  
+  '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  percentage = df['World Population Percentage'].values
   charts.generate_pie_chart(countries, percentage)
-  
-  
+
+  data = read_csv.read_csv('data.csv')
   country = input('Indicame un pais del mundo ')
   country = country.capitalize()
-
+  
   result = utils.population_by_country(data, country)
   
   if len(result) > 0:
